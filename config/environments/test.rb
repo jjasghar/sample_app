f69d22038e0b79cf561afbda617b6f4a354ee1ce
JJ Asghar <jj.asghar@peopleadmin.com>
2014-04-18 12:43:12 -0500

    completed sign_up

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,10 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # changing bcrypts alogrithem to speed up the tests
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name      "Jimmy Boye"
+    email     "jimmy@ex.com"
+    password  "foobar"
+    password_confirmation  "foobar"
+  end
+end
+
