52c4b0a5ffcaabdbb74d935b888f4e162d2406eb
JJ Asghar <jj.asghar@peopleadmin.com>
2014-05-21 13:24:46 -0500

    WIP: is a work in progress

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,7 +21,6 @@ describe "User Pages" do
     it { should have_selector('h1', text: user.name) }
     it { should have_selector('title', text: user.name) }
   end
-
   describe "signup" do
 
     before { visit signup_path }
@@ -67,5 +66,46 @@ describe "User Pages" do
     end
 
   end
+
+  describe "edit" do
+
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_selector('h1', text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name) { "New Name" }
+      let(:new_email) { "new@ex.com" }
+      before  do
+        fill_in "Name", with: new_name
+        fill_in "Email", with: new_email
+        fill_in "Password", with: user.password
+        fill_in "Confirm", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_selector('title', text: new_name ) }
+      it { should have_link('Sign out', href: signout_path ) }
+      it { should have_selector('div.alert.alert-success') }
+      specify { user.reload.name.should == new_name }
+      specify { user.reload.email.should == new_email }
+    end
+  end
+
+
 end
 
